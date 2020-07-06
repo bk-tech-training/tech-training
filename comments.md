54f06c227c23e270cf2b54bb71925a57fad9c86b
Betzalel Kenig <betzalelkenig@gmail.com>
2020-07-06 18:59:48 +0300

    add environmant

--- comments.md
+++ comments.md
@@ -7,5 +7,6 @@
  - `npm i font-awesome`
  - `ng add @angular/material`
  - `npm i firebase-tools -g`
+ - `npm add @angular/fire`
  
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDFofw5gzl-GSLlyQVkillheMGXIGULtxQ",
+    authDomain: "tech-training-3caf8.firebaseapp.com",
+    databaseURL: "https://tech-training-3caf8.firebaseio.com",
+    projectId: "tech-training-3caf8",
+    storageBucket: "tech-training-3caf8.appspot.com",
+    messagingSenderId: "1069297610424",
+    appId: "1:1069297610424:web:7d182de893eb68888dce4d",
+    measurementId: "G-442HC59YZX"
+  }
 };
